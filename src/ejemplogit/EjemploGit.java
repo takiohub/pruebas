d49527953141acbe9ccf9ec4e608d0bce0e4ca89
DAM17 <DAM17@203P01>
2015-04-21 13:51:32 +0200

    Version 5

---
+++ src/ejemplogit/EjemploGit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogit;
+
+/**
+ *
+ * @author DAM17
+ */
+public class EjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Comenzamos ya");
+        //System.out.println("Que tal");
+        System.out.println("Fin");
+    }
+    
+}
