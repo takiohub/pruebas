b1ade1bde02aa27ecc0abd1e93b129615550fa7e
DAM17 <DAM17@203P01>
2015-04-21 14:07:05 +0200

    Version 7

--- src/ejemplogit/EjemploGit.java
+++ src/ejemplogit/EjemploGit.java
@@ -20,8 +20,8 @@ public class EjemploGit {
         System.out.println("Fin");
     }
     
-    public void saluda(){
-        System.out.println("Hola");
+    public void despide(){
+        System.out.println("Adios");
     }
     
 }
